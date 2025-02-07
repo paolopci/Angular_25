2641558f836335e67528c9dcf9640bce69d3e5af
Paolo Paci <paolopci@yahoo.it>
2025-02-07 17:09:50 +0100

    applico la directiva

--- src/app/decorators/highlight-element06.directive.ts
+++ src/app/decorators/highlight-element06.directive.ts
@@ -1,9 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightElement06]'
 })
 export class HighlightElement06Directive {
+
+  @HostBinding('style.backgroundColor') bgColor = '#f2aa4cff';
+
+
 
   constructor() { }
 

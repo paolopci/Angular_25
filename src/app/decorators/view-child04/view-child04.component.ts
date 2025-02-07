eca276f79c4cc8af37b86a7896e4b3edef27ca38
Paolo Paci <paolopci@yahoo.it>
2025-02-07 16:09:52 +0100

    complete @ViewChild esempio ...

--- src/app/decorators/view-child04/view-child04.component.ts
+++ src/app/decorators/view-child04/view-child04.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-view-child04',
+  selector: 'app-child04',
   templateUrl: './view-child04.component.html',
   styleUrl: './view-child04.component.css'
 })
 export class ViewChild04Component {
 
+  count: number = 0;
+
+  incrementCounter() {
+    this.count++;
+  }
 }

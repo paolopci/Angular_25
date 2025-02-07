719b2683e3f31604b88123091706425b4c986818
Paolo Paci <paolopci@yahoo.it>
2025-02-07 15:06:59 +0100

    esempio uso @ViewChild()

---
+++ src/app/app04.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app04-root',
+  templateUrl: './app04.component.html',
+  styleUrl: './app04.component.css'
+})
+export class App04Component {
+
+}
---
+++ src/app/decorators/view-child04/view-child04.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-child04',
+  templateUrl: './view-child04.component.html',
+  styleUrl: './view-child04.component.css'
+})
+export class ViewChild04Component {
+
+}

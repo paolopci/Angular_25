3d97ce7ea6a5073713a8afe5a6c6af53cb8dd512
Paolo Paci <paolopci@yahoo.it>
2025-02-07 17:20:15 +0100

    applico più @hostBinding() con inizializzazione in onInit

--- src/app/decorators/highlight-element06.directive.ts
+++ src/app/decorators/highlight-element06.directive.ts
@@ -1,14 +1,20 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightElement06]'
 })
-export class HighlightElement06Directive {
+export class HighlightElement06Directive implements OnInit {
 
-  @HostBinding('style.backgroundColor') bgColor = '#f2aa4cff';
+  @HostBinding('style.backgroundColor') bgColor?: string;
+  @HostBinding('style.color') color?: string;
+  @HostBinding('style.fontWeight') boldFont?: string;
 
 
+  ngOnInit(): void {
+    this.bgColor = '#f2aa4cff';
+    this.color = 'white';
+    this.boldFont = '700';
+  }
 
-  constructor() { }
 
 }

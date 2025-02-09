8fde2fa378075cbe3f4326c0d041db41ed34ce5c
Paolo Paci <paolopci@yahoo.it>
2025-02-09 11:21:28 +0100

    apply my first custom pipe --> appendText

--- src/app/customPipes/pipes/append-text.pipe.ts
+++ src/app/customPipes/pipes/append-text.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AppendTextPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, suffixVal: string): string {
+    return value + suffixVal;
   }
 
 }
